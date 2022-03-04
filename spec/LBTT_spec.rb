8206560ac4d37790b2c70415de8a6750f84053f1
bmargerison <bmargerison@outlook.com>
2022-03-04 15:27:32 +0000

    Refactor to mock property class for LBTT tests

--- spec/LBTT_spec.rb
+++ spec/LBTT_spec.rb
@@ -1,17 +1,16 @@
 require 'LBTT'
-require 'property'
 
 describe LBTT do
 
+  let(:property) { double("property", :valuation => 150000) }
+
   it "initializes with tax and property" do
-    property = Property.new(150000)
     lbtt = LBTT.new(property)
     expect(lbtt.tax).to be()
     expect(lbtt.property).to be()
   end
 
   it "calculates the value of a property between 145k-150k" do
-    property = Property.new(150000)
     lbtt = LBTT.new(property)
     lbtt.calculate_tax
     expect(lbtt.tax).to be(100.00)
